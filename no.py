183a14b14bfe174d5d80d74788cf52a63c280d1c
pratham463 <nehagrg45@gmail.com>
2025-06-27 19:30:01 +0530

    Add files via upload

---
+++ no.py
@@ -0,0 +1,8 @@
+number = int(input("enter no. to check"))
+print("number to be checked:,number")
+if  number>0:
+ print("this is a positive no")
+elif  number<0:
+ print("this is a negative no")
+else:
+ print("this is a nuteral no")
