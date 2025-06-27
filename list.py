cee2ceac275dbca8f969c95bb9fe86059f00e9d5
pratham463 <nehagrg45@gmail.com>
2025-06-27 19:58:00 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,6 @@
+lst = ['apple','guava','mango','banana','kiwi']
+print("lenght of list:",len(lst))
+lst.append('Papaya')
+print ("updated list:",lst)
+lst.remove('Papaya')
+print ("updated list:",lst)
