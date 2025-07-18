9beb1344b8b59af3e24f1a88276b276c4df5c9dc
pratham463 <nehagrg45@gmail.com>
2025-07-18 19:49:04 +0530

    Add files via upload

---
+++ demolition.py
@@ -0,0 +1,10 @@
+def no_notes(a):
+    Q= [2000, 500, 100, 200, 50, 20, 10]
+    x =0
+    for i in range(7):
+        q = Q[i]
+        x = a//q
+        print("Notes of {} = {}".format(q,x))
+        a = a%q
+amount = int(input("Enter Total Amount"))
+no_notes(amount)
