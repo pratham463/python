55d934831431a6c07f80114385a0ce08b9b296e8
pratham463 <nehagrg45@gmail.com>
2025-08-09 16:20:43 +0530

    Add files via upload

---
+++ ACP.PY
@@ -0,0 +1,8 @@
+file_read = open('codingal.txt','r')
+print("file in read mode")
+print(file_read.read())
+file_read.close()
+file_write = open('codingal.txt','w')
+file_write.write("file in write mode")
+file_write.write(" i love skating")
+file_write.close()
